01e65d6c37f01060d9bbeb725ebf5fd0147ca26f
evelynhelena <evelynhelena781@gmail.com>
2022-05-10 09:59:13 -0300

    finalizando a aplicação

--- src/useCases/submitFeedbackUseCases.ts
+++ src/useCases/submitFeedbackUseCases.ts
@@ -37,6 +37,7 @@ export class SubmitFeedbackUseCases {
                 `<div style="font-family: sans-serif; font-size:16px; color:#111;">`,
                 `<p>Tipo do feedback: ${type}</p>`,
                 `<p>Comentário: ${comment}</p>`,
+                screenshot ? `<img src="${screenshot}">` : ``,
                 `</div>`,
             ].join('\n')
         })
